8efa60d634a33d8f1faa1eb4eb38c7cd51d08eb2
Matthew Gleich <email@mattglei.ch>
2020-12-27 17:27:00 -0500

    🎉 Initial commit

---
+++ Nuke/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Nuke
+//
+//  Created by Matthew Gleich on 12/27/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
