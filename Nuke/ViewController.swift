3395dfc6448e8231cf0f160437039cc2ab6c3e6c
Matthew Gleich <email@mattglei.ch>
2020-12-27 17:42:13 -0500

    🎨 Format with xcformat

--- Nuke/ViewController.swift
+++ Nuke/ViewController.swift
@@ -8,7 +8,6 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,10 +16,7 @@ class ViewController: NSViewController {
 
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
 }
-
