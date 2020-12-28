eb154731116aa3e19766bbfd1022e95391eb7fb7
Matthew Gleich <email@mattglei.ch>
2020-12-27 23:13:09 -0500

    ✨ Add basic popover

--- Nuke/AppDelegate.swift
+++ Nuke/AppDelegate.swift
@@ -9,11 +9,31 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.button?.title = "Nuke"
+        statusItem.button?.target = self
+        statusItem.button?.action = #selector(showSettings)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+
+    @objc func showSettings() {
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        guard let vc = storyboard.instantiateController(withIdentifier: "ViewController") as? ViewController else {
+            fatalError("Unable to find a story board with ID of: ViewController")
+        }
+
+        guard let button = statusItem.button else {
+            fatalError("Couldn't find the status item button.")
+        }
+
+        let popoverView = NSPopover()
+        popoverView.contentViewController = vc
+        popoverView.behavior = .transient
+        popoverView.show(relativeTo: button.bounds, of: button, preferredEdge: .maxY)
     }
 }
--- Nuke/ViewController.swift
+++ Nuke/ViewController.swift
@@ -10,13 +10,14 @@ import Cocoa
 class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
         didSet {
-            // Update the view, if already loaded.
         }
+    }
+
+    @IBAction func nukePressed(_ sender: NSButton) {
+        print("Hello World!")
     }
 }
